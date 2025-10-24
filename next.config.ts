96c0a5c4febd3ebfc27ddf0bd2009be80079a00b
Shawn <safesprayteam@gmail.com>
2025-10-24 09:09:58 -0700

    Add trailingSlash true to next.config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ import { NextConfig } from 'next'; // Import the type
 const nextConfig: NextConfig = { // Add the type annotation
   // Add this line to force a static export:
   output: 'export',
+  // Add this line to help with static routing:
+  trailingSlash: true,
   // Recommended: Disable image optimization for static export if not using a compatible loader
   images: {
     unoptimized: true,
